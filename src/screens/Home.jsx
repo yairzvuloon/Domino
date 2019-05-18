6dbf42efb18d49651563e93388d167c1c0da6ffc
Yair Zvuloon <yairzv0@gmail.com>
2019-05-19 00:27:52 +0300

    move some home logic to utils

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,8 +1,8 @@
-
 import React from "react";
 import Board from "../components/Board.jsx";
 import Cart from "../components/Cart.jsx";
-import HomeStyle from "../style/HomeStyle.css";
+import "../style/HomeStyle.css";
+import {DominoBoxLogic} from '../utilities/HomeUtility';
 
 const createEmptyBoard = size => {
   let matrix = new Array(size);
@@ -22,63 +22,7 @@ const setInitialBoard = size => {
   return board;
 };
 
-const createShuffledArray = size => {
-  let a = new Array(size);
-  for (let i = 0; i < size; i++) {
-    a[i] = i;
-  }
-  for (let i = a.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [a[i], a[j]] = [a[j], a[i]];
-  }
-  return a;
-};
 
-const createCardsArray = () => {
-  let arr = new Array(28);
-  let arrIndex = 0;
-  for (let i = 0; i < 7; i++) {
-    for (let j = i; j < 7; j++) {
-      arr[arrIndex] = { valid: undefined, side1: i, side2: j };
-      console.log(arr[arrIndex]);
-      arrIndex++;
-    }
-  }
-  return arr;
-};
-
-const setInitialCart = i_DominoBox => {
-  let cart = new Array(6);
-  for (let i = 0; i < 7; i++) {
-    cart[i] = i_DominoBox.getCard();
-    console.log("cart[" + i + "]: " + cart[i]);
-  }
-  return cart;
-};
-
-class DominoBox {
-  constructor() {
-    this.indexesCardsBox = createShuffledArray(28);
-    this.indexesCardsBoxIndex = 27;
-    this.cardsArray = createCardsArray();
-  }
-  print() {
-    for (let i = 0; i < 28; i++) {
-      console.log(this.indexesCardsBox[i]);
-    }
-  }
-  getCard() {
-    let ret = null;
-    if (this.indexesCardsBoxIndex > 0) {
-      let cardIndex = this.indexesCardsBox.pop();
-      ret = this.cardsArray[cardIndex];
-      this.indexesCardsBoxIndex--;
-      console.log("in getCard()");
-      console.log("card: " + ret.side1 + ", " + ret.side2);
-    }
-    return ret;
-  }
-}
 class Card {
   constructor(i_Valid, i_Side1, i_Side2, i_IsLaying) {
     this.valid = i_Valid;
@@ -87,13 +31,13 @@ class Card {
     this.isLaying = i_IsLaying;
   }
 }
+
+
 class Home extends React.Component {
   constructor(props) {
     super(props);
-    this.cardsBox = new DominoBox();
-    //this.cardsBox.print();
     const initialBoard = setInitialBoard(57);
-    const initialCart = setInitialCart(this.cardsBox);
+    const initialCart = this.setInitialCart();
     this.state = {
       boardMap: initialBoard,
       cartMap: initialCart,
@@ -101,24 +45,13 @@ class Home extends React.Component {
     };
     this.validLocationsByNumber = [...Array(7)].map(x => Array(0));
   }
-
-  render() {
-    return (
-      <div id="homeContainer">
-        <div id="boardFrame">
-          <Board
-            cells={this.state.boardMap}
-            onClick={(i, j) => this.handleBoardClick(i, j)}
-          />
-        </div>
-        <Cart
-          id="cartStyle"
-          cart={this.state.cartMap}
-          onClick={(i, value) => this.handleCartClick(i, value)}
-        />
-      </div>
-    );
-  }
+   setInitialCart(){
+    let cart = new Array(6);
+    for (let i = 0; i < 7; i++) {
+      cart[i] = DominoBoxLogic.getCard();
+    }
+    return cart;
+  };
   isEmptyAndNotValid(row, col) {
     const { boardMap } = this.state;
     return (
@@ -218,12 +151,12 @@ class Home extends React.Component {
       let card = new Card(false, side1, side2, true);
       if (
         boardMap[row][col + 1].side1 === side1 ||
-        boardMap[row][col + 1].side2 === side2||
+        boardMap[row][col + 1].side2 === side2 ||
         boardMap[row][col - 1].side1 === side1 ||
         boardMap[row][col - 1].side2 === side2 ||
         boardMap[row - 1][col].side1 === side1 ||
         boardMap[row - 1][col].side2 === side2 ||
-        boardMap[row + 1][col].side1 === side1||
+        boardMap[row + 1][col].side1 === side1 ||
         boardMap[row + 1][col].side2 === side2
       ) {
         card = new Card(false, side2, side1, true);
@@ -246,6 +179,24 @@ class Home extends React.Component {
       cartMap: newCartMap,
       selectedCard: value
     }));
+  }
+
+  render() {
+    return (
+      <div id="homeContainer">
+        <div id="boardFrame">
+          <Board
+            cells={this.state.boardMap}
+            onClick={(i, j) => this.handleBoardClick(i, j)}
+          />
+        </div>
+        <Cart
+          id="cartStyle"
+          cart={this.state.cartMap}
+          onClick={(i, value) => this.handleCartClick(i, value)}
+        />
+      </div>
+    );
   }
 }
 
---
+++ src/utilities/HomeUtility.js
@@ -0,0 +1,50 @@
+class DominoBox {
+  constructor() {
+    this.indexesCardsBox = this.createShuffledArray(28);
+    this.indexesCardsBoxIndex = 27;
+    this.cardsArray = this.createCardsArray();
+  }
+  print() {
+    for (let i = 0; i < 28; i++) {
+      console.log(this.indexesCardsBox[i]);
+    }
+  }
+  getCard() {
+    let ret = null;
+    if (this.indexesCardsBoxIndex > 0) {
+      let cardIndex = this.indexesCardsBox.pop();
+      ret = this.cardsArray[cardIndex];
+      this.indexesCardsBoxIndex--;
+      console.log("in getCard()");
+      console.log("card: " + ret.side1 + ", " + ret.side2);
+    }
+    return ret;
+  }
+
+  createShuffledArray(size) {
+    let a = new Array(size);
+    for (let i = 0; i < size; i++) {
+      a[i] = i;
+    }
+    for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  }
+
+  createCardsArray() {
+    let arr = new Array(28);
+    let arrIndex = 0;
+    for (let i = 0; i < 7; i++) {
+      for (let j = i; j < 7; j++) {
+        arr[arrIndex] = { valid: undefined, side1: i, side2: j };
+        console.log(arr[arrIndex]);
+        arrIndex++;
+      }
+    }
+    return arr;
+  }
+}
+
+export const DominoBoxLogic = new DominoBox();
